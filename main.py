e35b6bed35c596c3371d14d0c4d8fe9e00b30aac
gordonrl <gordonrl@umich.edu>
2023-05-16 18:04:39 +0100

    Made global variable names for each question on the form

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import csv
+
+#REMEMBER: People in one car are less likely to be in another
+#CHECK DUES PAYING MEMBERS!!!!!!
+
+#In order to make the form well-written for people filling it out 
+#some of the dictionary keys (questions on the form) are long
+#So here are some global variables to make thing shorter
+
+#I know there's a bunch of them but it makes things easier
+
+uniqname = "Uniqname"
+name = "Name"
+number = "Phone Number"
+driving_this_week = "Will you be driving on at least 1 day this week?"
+car_info = "Car model and color"
+car_capacity = "How many people can you fit?"
+tues_choice = "Will you be driving or riding on Tuesday?"
+tues_loc = "Where are you leaving from on Tuesday?"
+thurs_choice = "Will you be driving or riding on Thursday?"
+thurs_loc = "Where are you leaving from on Thursday?"
+sun_choice = "Will you be driving or riding on Sunday?"
+sun_loc = "Where are you leaving from on Sunday?"
